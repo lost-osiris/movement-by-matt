4a2445bf622e69018a50eb27e1530d44e57b8cc4
Matthew Owens <matt@mowens.io>
2024-02-03 19:29:38 -0700

    now using vercel system varables

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -6,18 +6,13 @@ import HomeIcon from '@mui/icons-material/Home'
 import HeaderUser from './HeaderUser'
 
 const Header = () => {
-  const homeUrl =
-    process.env.NODE_ENV === 'production'
-      ? `${process.env.BASE_PATH}/index.html`
-      : '/'
-
   return (
     <AppBar
       position='fixed'
       sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
     >
       <Toolbar>
-        <Fab onClick={() => (window.location.href = homeUrl)} size='small'>
+        <Fab onClick={() => (window.location.href = '/')} size='small'>
           <HomeIcon />
         </Fab>
         <Typography component='div' sx={{ flexGrow: 1, pl: 1 }} variant='h6'>
--- webpack.js
+++ webpack.js
@@ -13,7 +13,7 @@ const devConfig = require('./webpack.dev.js')
 const devMode = process.env.NODE_ENV !== 'production'
 const publicPath = '/'
 
-const dotEnvConfig = require('dotenv').config()
+const dotEnvConfig = require('dotenv').config({ systemvars: devMode })
 
 const commonConfig = {
   context: resolve(__dirname, 'src/'),
